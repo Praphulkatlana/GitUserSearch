ea160a90a8e9c417eb62d034c025bf83ee488c05
PraphulKatlana <nikunj@Nikunj-MacBook.local>
2021-09-18 00:14:53 +0530

    Code Clean

--- src/Component/repos.js
+++ src/Component/repos.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
-import {Link} from "react-router-dom"
 import Axios from 'axios'
 const  Repos=({ repos_url }) =>{
     const [repos, setrepos] = useState([])
